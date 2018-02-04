8ec8b9e9a6013038679b10c7ce6c457ae3b39790
Fernando Olivero <fernando.olivero.mail@gmail.com>
2018-02-04 09:40:42 -0500

    Drop local carthage support

--- RoundedButtonImageRenderer/RoundedButtonImageRenderer.h
+++ RoundedButtonImageRenderer/RoundedButtonImageRenderer.h
@@ -1,19 +1,7 @@
-//
-//  RoundedButtonImageRenderer.h
-//  RoundedButtonImageRenderer
-//
-//  Created by Fernando Olivero on 26/03/2017.
-//  Copyright © 2017 Fernando Olivero. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-//! Project version number for RoundedButtonImageRenderer.
 FOUNDATION_EXPORT double RoundedButtonImageRendererVersionNumber;
-
-//! Project version string for RoundedButtonImageRenderer.
 FOUNDATION_EXPORT const unsigned char RoundedButtonImageRendererVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <RoundedButtonImageRenderer/PublicHeader.h>
 
 
